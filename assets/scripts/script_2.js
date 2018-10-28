179f42af5dbc94b60bb45b7588fd6b23dd8d93da
dimitratompoulidou <p15tobo@ionio.gr>
2018-10-28 17:31:23 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -11,7 +11,7 @@ var margin = {top: 30, right: 0, bottom: 20, left: 0},
     formatNumber = d3.format(","),
     transitioning;
 
-var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#9400D3", "#FF1493", "#008000", "#778899"]);
+var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#cc33ff", "#ff0066", "#006600", "#c2c2a3"]);
 
 // sets x and y scale to determine size of visible boxes
 var x = d3.scaleLinear()
